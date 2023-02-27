fa549c055bc1fcc80f46c7b6e60ea6ccdc7d5762
Kwang <punyarit.thi@hlabconsulting.com>
2023-02-27 17:12:57 +0700

    fix ts

--- cx/DatePicker.ts
+++ cx/DatePicker.ts
@@ -2,7 +2,7 @@ import { createComponent, EventName } from '@lit-labs/react';
 import * as React from 'react';
 import { DatePicker } from '@cortex-ui/core/cx/datepicker';
 
-export const CxModal = createComponent({
+export const CXDatePicker = createComponent({
   tagName: 'cx-datepicker',
   elementClass: DatePicker,
   react: React,
