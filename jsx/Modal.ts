2b0c475ebfdf4d54d549ab81b04b8161ab8104cb
Kwang <punyarit.thi@hlabconsulting.com>
2023-03-20 21:57:46 +0700

    fix v

--- jsx/Modal.ts
+++ jsx/Modal.ts
@@ -1,9 +1,15 @@
 import { createComponent } from '@lit-labs/react';
 import * as React from 'react';
-import { Modal } from '@cortex-ui/core/cx/components/modal/modal';
+import { Modal } from '@cortex-ui/core/cx/modal';
 
 export const CxModal = createComponent({
   tagName: 'cx-modal',
   elementClass: Modal,
   react: React,
 });
+
+declare global {
+  namespace CXModal {
+    type JSX = typeof CxModal.defaultProps;
+  }
+}
